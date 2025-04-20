607154efcb93b0a2c2c3c7b80f1e8f404b3dde22
tivvvv <18612553935@163.com>
2025-04-20 10:19:12 +0800

    feat: 补充ArrayList单元测试.

---
+++ src/test/java/com/tiv/list/ListTest.java
@@ -0,0 +1,23 @@
+package com.tiv.list;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ListTest {
+
+    @Test
+    void testArrayList() {
+        List<String> list = new MyArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add(String.valueOf(i));
+        }
+        assertEquals(20, list.size());
+        list.remove(4);
+        list.remove("18");
+        assertEquals(18, list.size());
+        assertEquals("5", list.get(4));
+        assertEquals("19", list.get(17));
+        list.forEach(System.out::println);
+    }
+}
