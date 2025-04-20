e88798264254b8afbe0830430d125389fec990d5
tivvvv <18612553935@163.com>
2025-04-20 19:22:07 +0800

    feat: 实现LinkedList add操作.

--- src/main/java/com/tiv/list/MyLinkedList.java
+++ src/main/java/com/tiv/list/MyLinkedList.java
@@ -6,14 +6,56 @@ public class MyLinkedList<E> implements List<E> {
 
     private int size;
 
+    private Node<E> head;
+
+    private Node<E> tail;
+
     @Override
     public void add(E e) {
-
+        Node<E> node = new Node<>(tail, null, e);
+        if (tail != null) {
+            tail.next = node;
+        } else {
+            head = node;
+        }
+        tail = node;
+        size++;
     }
 
     @Override
     public void add(E e, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == size) {
+            add(e);
+            return;
+        }
+        Node<E> indexNode = findNode(index);
+        Node<E> node = new Node<>(indexNode.pre, indexNode, e);
+        if (indexNode.pre == null) {
+            head = node;
+        } else {
+            indexNode.pre.next = node;
+        }
+        indexNode.pre = node;
+        size++;
+    }
 
+    private Node<E> findNode(int index) {
+        Node<E> result = null;
+        if (index < size / 2) {
+            result = head;
+            for (int i = 0; i < index; i++) {
+                result = result.next;
+            }
+        } else {
+            result = tail;
+            for (int i = size - 1; i > index; i--) {
+                result = result.pre;
+            }
+        }
+        return result;
     }
 
     @Override
@@ -53,6 +95,12 @@ public class MyLinkedList<E> implements List<E> {
         E value;
 
         public Node(E value) {
+            this.value = value;
+        }
+
+        public Node(Node<E> pre, Node<E> next, E value) {
+            this.pre = pre;
+            this.next = next;
             this.value = value;
         }
     }
